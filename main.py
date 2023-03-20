675bdb29b32d6a9c9790c50f16beeb53bb464b21
Ashish Christian <60458780+Ashu-spec@users.noreply.github.com>
2023-03-20 16:50:15 +0000

    cv

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ elif is_magician and not is_expert:
   print("Atleast Your Getting There")
 
 elif not is_magician:
-  print("You Are Not A Master Magician")
+  print("You Are Not A Master Magician")nkjnk
